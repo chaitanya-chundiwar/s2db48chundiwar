c171f87e8ec1799094e6cd338b93d3545df9869f
chaitanya-chundiwar <chaitanya833krishna@gmail.com>
2022-04-13 21:54:26 -0500

    commit

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const connectionString = "mongodb+srv://demo:demo@cluster0.wuvl3.mongodb.net/learnMongo?retryWrites=true&w=majority";
+mongoose = require('mongoose');
+mongoose.connect(connectionString,
+  { useNewUrlParser: true, useUnifiedTopology: true });
+
+var Costume = require("./models/costume");
+var Food = require("./models/food");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var beveragesRouter = require('./routes/beverages');
+var foodsRouter = require('./routes/foods');
 var addmodsRouter = require('./routes/addmods');
-var selectorsRouter = require('./routes/selectors');
+var selectorRouter = require('./routes/selector');
+var resourceRouter = require('./routes/resource');
 var app = express();
 
 // view engine setup
@@ -23,17 +31,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/beverages', beveragesRouter);
+app.use('/foods', foodsRouter);
 app.use('/addmods', addmodsRouter);
-app.use('/selectors', selectorsRouter);
+app.use('/selector', selectorRouter)
+app.use('/resource', resourceRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -44,3 +53,68 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+/// We can seed the collection if needed on server start
+async function recreateDB() {
+  // Delete everything
+  await Costume.deleteMany();
+
+  let instance1 = new Costume({ costume_type: "ShakthiMan", size: 'large', cost: 25.2 });
+  instance1.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("First object saved in Costume")
+  });
+
+  let instance2 = new Costume({ costume_type: "Moon Knight", size: 'small', cost: 18.1 });
+  instance2.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Second object saved in Costume")
+  });
+
+  let instance3 = new Costume({ costume_type: "Black Panther", size: 'medium', cost: 12.4 });
+  instance3.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Third object saved in Costume")
+  });
+
+  let instance4 = new Costume({ costume_type: "Scarlett Witch", size: 'large', cost: 22 });
+  instance4.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Fourth object saved in Costume")
+  });
+
+// Delete everything in Food
+  await Food.deleteMany();
+
+  let instance5 = new Food({ food_brand: "Dominos", food_color: 'Blue', food_cost: 20 });
+  instance5.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("First object saved in Food")
+  });
+
+  let instance6 = new Food({ food_brand: "KFC", food_color: 'Red', food_cost: 15 });
+  instance6.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Second object saved in Food")
+  });
+
+  let instance7 = new Food({ food_brand: "Taco bells", food_color: 'Yellow', food_cost: 17 });
+  instance7.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Third object saved in Food")
+  });
+
+  let instance8 = new Food({ food_brand: "Sonic", food_color: 'Violet', food_cost: 18 });
+  instance8.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Fourth object saved in Food")
+  });
+}
+let reseed = true;
+if (reseed) { recreateDB(); }
+
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once("open", function () { console.log("Connection to DB succeeded") });
